6561e4f9241b95dc924c49b827e38f5d1a5f6d29
sergio silva <sergio.silva@dtemite.cl>
2026-04-17 21:14:43 -0400

    cambios urls

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 window.ENV = {
-  "API_PAYMENT_URL": "http://localhost:3010",
-  "API_REGISTER_URL": "http://localhost:3000/api/public/register-client",
-  "DASHBOARD_URL": "http://localhost:5500/"
+  "API_PAYMENT_URL": "https://webpay.diffsii.cl",
+  "API_REGISTER_URL": "https://saas.diffsii.cl/api/public/register-client",
+  "DASHBOARD_URL": "https://saas.diffsii.cl/"
 };
--- generate-env.js
+++ generate-env.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 const envVars = {
-    API_PAYMENT_URL: process.env.API_PAYMENT_URL || "http://localhost:3010",
-    API_REGISTER_URL: process.env.API_REGISTER_URL || "http://localhost:3000/api/public/register-client",
-    DASHBOARD_URL: process.env.DASHBOARD_URL || "http://localhost:5500/"
+    API_PAYMENT_URL: process.env.API_PAYMENT_URL || "https://webpay.diffsii.cl",
+    API_REGISTER_URL: process.env.API_REGISTER_URL || "https://saas.diffsii.cl/api/public/register-client",
+    DASHBOARD_URL: process.env.DASHBOARD_URL || "https://saas.diffsii.cl/"
 };
 
 const content = `window.ENV = ${JSON.stringify(envVars, null, 2)};`;
